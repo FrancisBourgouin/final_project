385033841d44405132ec2d7a9aedd3e03a58508a
Francis Bourgouin <netbeuix@gmail.com>
2022-01-13 12:01:46 -0500

    added basic routes for candidates and interviewers

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,38 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/api/candidates", (req, res) => {
+  const candidates = [{ name: "Martin" }, { name: "Tania" }, { name: "Mojdeh" }];
+
+  res.json(candidates);
+});
+
+app.get("/api/candidates/:candidate_id", (req, res) => {
+  const candidate = { name: "Martin" };
+
+  res.json(candidate);
+});
+
+app.get("/api/interviewers", (req, res) => {
+  const interviewers = [{ name: "Martin" }, { name: "Tania" }, { name: "Mojdeh" }];
+
+  res.json(interviewers);
+});
+
+app.get("/api/interviewers/:interviewer_id", (req, res) => {
+  const interviewer = { name: "Martin" };
+
+  res.json(interviewer);
+});
 
 module.exports = app;
